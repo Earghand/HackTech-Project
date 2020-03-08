8e8752b176f9cfbbb11ac9dc00d5ff0c8dea4222
kar731 <sean.jinbo1@gmail.com>
2020-03-08 04:27:08 -0700

    Working webserver. needs css

--- webserver/web.py
+++ webserver/web.py
@@ -24,10 +24,35 @@ def get_locations(location, coords, keywords):
     places = {}
 
     for key in keywords:
-        place = gmaps.find_place(input = [f"{location} {key}"], input_type="textquery", fields=return_fields, location_bias=bias)
-        places[key] = place
+        #place = gmaps.find_place(input = [f"{location} {key}"], input_type="textquery", fields=return_fields, location_bias=bias)
+        place = gmaps.places_nearby(location=coords, radius=16000, keyword=key, rank_by="prominence")
+        places[key] = place["results"][0]
+        #print(place["results"])
 
     return places
+
+def places_list(set_of_locations):
+    returned_locations = []
+
+    for i in set_of_locations:
+        for key in i:
+            place = i[key]
+
+            if place == "OK":
+                break
+
+            location_coords = (place["geometry"]["location"]["lat"], place["geometry"]["location"]["lng"])
+            html = "<b>{name}</b><br><b>Rating: </b>{rating}<br><a href='https://www.google.com/maps/search/?api=1&query_place_id={place_id}'>Google Maps</a>"
+
+            returned_locations.append({
+                'icon': 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+                'lat': location_coords[0], 'lng': location_coords[1],
+                'infobox': html.format(
+                    name=place["name"],
+                    rating=place["rating"],
+                    place_id=place["place_id"]
+                )})
+    return returned_locations
 
 @app.route("/", methods=['GET', 'POST'])
 def mapview():
@@ -36,43 +61,38 @@ def mapview():
         coords = gmaps.geocode(address=location)[0]
         coords = (coords["geometry"]["location"]["lat"], coords["geometry"]["location"]["lng"])
 
-        morning = get_locations(location, coords, ["breakfast", "coffee", "park"])
-        afternoon = get_locations(location, coords, [])
-        evening = get_locations(location, coords, [])
+        morning = get_locations(location, coords, ["breakfast", "coffee", "park", "hike"])
+        afternoon = get_locations(location, coords, ["lunch", "zoo", "museum", "shopping"])
+        evening = get_locations(location, coords, ["dinner", "dessert", "club", "concert", "nightlife"])
 
-        locations = []
+        locations = places_list((morning, afternoon, evening))
 
-        for key in morning:
-            for place in morning[key]:
-                place = morning[key][place]
-
-                if place == "OK":
-                    break
-
-                location_coords = gmaps.geocode(address=place[0]["formatted_address"])
-                location_coords = (location_coords[0]["geometry"]["location"]["lat"], location_coords[0]["geometry"]["location"]["lat"])
-                html = "<b>{name}</b><br><b>Rating: </b>{rating}<br>{gmaps_link}Google Maps</a>"
-
-                locations.append({
-                    'icon': 'https://maps.google.com/mapfiles/ms/icons/green-dot.png',
-                    'lat': location_coords[0], 'lng': location_coords[1],
-                    'infobox': html.format(
-                        name=place[0]["name"],
-                        rating=place[0]["rating"],
-                        gmaps_link=place[0]["photos"][0]["html_attributions"][0].replace("</a>", '')
-                    )})
-
-         # creating a map in the view
+        # creating a map in the view
         mymap = Map(
             identifier="mymap",
             lat=coords[0],
             lng=coords[1],
             markers=locations,
-            style = "height:30rem;width:75rem",
-            zoom = 15,
+            cls="maps",
+            style = ("height:100rem;", "width:100rem;", "margin-left:2rem;", "margin-bottom:4rem;")
+            #style = ("height:1000px","width:1000px", "top:17%;", "left:3%;", "right:39.5%;", "bottom:4%;"),
+            #zoom = 12,
         )
-        return render_template('template.html', mymap=mymap)
 
+        locations_template = "<div class='location-element'> <b>{name}</b><br><b>Rating: </b>{rating}/5<br>{address}</div>"
+        location_html = ""
+
+        for i in (morning, afternoon, evening):
+            for key in i:
+                place = i[key]
+
+                location_html += locations_template.format(
+                    name = place["name"],
+                    rating = place["rating"],
+                    address = place["vicinity"],
+                )
+
+        return render_template('template.html', mymap=mymap, locations=location_html)
 
     else:
         return render_template('template_home.html')
